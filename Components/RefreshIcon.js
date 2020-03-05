1972c939b98b1dc56acb084e2b4881c5da01198a
Austin Meyer <frizzkitten@gmail.com>
2020-03-05 13:23:20 -0600

    nice animation down after refresh

--- Components/RefreshIcon.js
+++ Components/RefreshIcon.js
@@ -10,32 +10,56 @@ const ICON_COLOR = "black";
 // refreshing: whether should show a "currently updating" icon
 // visibility: how close you are to starting a refresh. anything
 // above 1 indicates that you are at a max
-export default ({ refreshing, visibility }) => {
-    const scale = visibility < 0 ? 0 : visibility > 1 ? 1 : visibility;
+export default class RefreshIcon extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { marginTop: new Animated.Value(-24) };
+    }
 
-    return (
-        <View>
-            {refreshing ? (
-                <Animated.View style={styles.refreshing}>
+    componentDidUpdate(prevProps) {
+        if (prevProps.refreshing && !this.props.refreshing)
+            this._start_disappear_animation();
+        else if (!prevProps.refreshing && this.props.refreshing)
+            this.setState({ marginTop: new Animated.Value(30) });
+    }
+
+    _start_disappear_animation = () => {
+        Animated.timing(this.state.marginTop, {
+            toValue: -24,
+            duration: 300
+        }).start();
+    };
+
+    render() {
+        const { refreshing, visibility } = this.props;
+
+        const scale = visibility < 0 ? 0 : visibility > 1 ? 1 : visibility;
+        const height = 24;
+        const marginTop = this.state.marginTop || -24;
+
+        return (
+            <Animated.View
+                style={
+                    { height, marginTop }
+                    /*refreshing
+                        ? styles.refreshing
+                        : visibility <= 0
+                        ? styles.invisible
+                        : styles.not_refreshing*/
+                }
+            >
+                {refreshing ? (
                     <RotateCW stroke={ICON_COLOR} />
-                </Animated.View>
-            ) : (
-                <View
-                    style={
-                        visibility <= 0
-                            ? styles.invisible
-                            : styles.not_refreshing
-                    }
-                >
+                ) : (
                     <DownArrow
                         stroke={ICON_COLOR}
                         style={{ transform: [{ scale }], opacity: scale }}
                     />
-                </View>
-            )}
-        </View>
-    );
-};
+                )}
+            </Animated.View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     refreshing: { height: 24, marginTop: 30 },
