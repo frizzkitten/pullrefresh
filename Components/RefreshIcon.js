effcdc4402b999536067b6a5ad62eced3caca452
Austin Meyer <frizzkitten@gmail.com>
2020-03-05 14:21:29 -0600

    cleaned up RefreshIcon

--- Components/RefreshIcon.js
+++ Components/RefreshIcon.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, Text, StyleSheet, Animated } from "react-native";
+import { View, Text, Animated } from "react-native";
 import DownArrow from "./SVG/DownArrow";
 import RotateCW from "./SVG/RotateCW";
 
@@ -17,12 +17,14 @@ export default class RefreshIcon extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
+        // when refreshing stops, smoothly transition out the spinning arrow
         if (prevProps.refreshing && !this.props.refreshing)
             this._start_disappear_animation();
         else if (!prevProps.refreshing && this.props.refreshing)
             this.setState({ marginTop: new Animated.Value(30) });
     }
 
+    // initiate the animation the removes the spinning arrow
     _start_disappear_animation = () => {
         Animated.timing(this.state.marginTop, {
             toValue: -24,
@@ -34,20 +36,10 @@ export default class RefreshIcon extends React.Component {
         const { refreshing, visibility } = this.props;
 
         const scale = visibility < 0 ? 0 : visibility > 1 ? 1 : visibility;
-        const height = 24;
         const marginTop = this.state.marginTop || -24;
 
         return (
-            <Animated.View
-                style={
-                    { height, marginTop }
-                    /*refreshing
-                        ? styles.refreshing
-                        : visibility <= 0
-                        ? styles.invisible
-                        : styles.not_refreshing*/
-                }
-            >
+            <Animated.View style={{ marginTop }}>
                 {refreshing ? (
                     <RotateCW stroke={ICON_COLOR} />
                 ) : (
@@ -60,9 +52,3 @@ export default class RefreshIcon extends React.Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    refreshing: { height: 24, marginTop: 30 },
-    not_refreshing: { height: 24, marginTop: -24 },
-    invisible: { display: "none" }
-});
