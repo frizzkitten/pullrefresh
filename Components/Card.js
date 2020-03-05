3416464a1d9b80fc33637ec7bf7b4977d5cda4e0
Austin Meyer <frizzkitten@gmail.com>
2020-03-05 13:30:46 -0600

    making Cards animated in - not done yet

--- Components/Card.js
+++ Components/Card.js
@@ -1,12 +1,35 @@
 import React from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Animated } from "react-native";
 
-export default ({ text }) => (
-    <View style={styles.container}>
-        <Text style={styles.text}>{text}</Text>
-    </View>
-);
+const height = 194;
+
+export default class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { marginTop: new Animated.Value(30) };
+    }
+
+    // componentDidMount() {
+    //     // Animated.timing(this.state.marginTop, {
+    //     //     toValue: 30,
+    //     //     duration: 500
+    //     // }).start();
+    // }
+
+    render() {
+        return (
+            <View
+                style={{
+                    ...styles.container,
+                    marginTop: this.state.marginTop
+                }}
+            >
+                <Text style={styles.text}>{this.props.text}</Text>
+            </View>
+        );
+    }
+}
 
 const styles = {
     container: {
@@ -14,11 +37,10 @@ const styles = {
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.15,
         shadowRadius: 12.5,
-        height: 194,
+        height,
         width: 315,
         borderRadius: 10,
         backgroundColor: "#fff",
-        marginTop: 30,
         padding: 30
     },
     text: {
